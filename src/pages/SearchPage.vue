07c835153f3b341310debc5d2ab8135c3c10cb3f
yuval5210 <yucoh@post.bgu.ac.il>
2022-06-26 22:11:14 +0300

    Search function

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="container">
     <h1 class="title">Search Page</h1>
-    <RecipePreviewList title="Search Recipe">
-      <b-form class="form-inline" @submit.prevent="onSearch">
-        <b-form-group class="input-group mb-3">
-          <b-form-input
-            id="input-group-search"
-            type="text"
-            v-model="query"
-            class="form-control"
-            placeholder="Find a Recipe"
-            aria-label="Find a Recipe"
-            aria-describedby="button-addon2"
-          />
-          <b-form-invalid-feedback>
-            Recipe's name is required
-          </b-form-invalid-feedback>
-          <div class="input-group-append">
-            <b-button size="" class="mb-3" type="submit">
-              <b-icon icon="search" aria-hidden="true"></b-icon>
-            </b-button>
-          </div>
-        </b-form-group>
-      </b-form>
-    </RecipePreviewList>
+    <b-form class="form-inline" @submit.prevent="onSearch">
+      <b-form-group class="input-group mb-3">
+        <b-form-input
+          id="input-group-search"
+          type="text"
+          v-model="query"
+          class="form-control"
+          placeholder="Find a Recipe"
+          aria-label="Find a Recipe"
+          aria-describedby="button-addon2"
+        />
+        <b-form-invalid-feedback>
+          Recipe's name is required
+        </b-form-invalid-feedback>
+        <div class="input-group-append">
+          <b-button size="" class="mb-3" type="submit">
+            <b-icon icon="search" aria-hidden="true"></b-icon>
+          </b-button>
+        </div>
+      </b-form-group>
+    </b-form>
+    <RecipePreviewList title="Search Recipe"></RecipePreviewList>
   </div>
 </template>
 
 <script>
+import { BIconHandThumbsDown } from "bootstrap-vue";
 import RecipePreviewList from "../components/RecipePreviewList.vue";
 export default {
   name: "Search",
@@ -37,12 +37,12 @@ export default {
 
   data() {
     return {
-        query: "",
-        number: 5,
-        cuisine: "",
-        diet: "",
-        intolerances: "",
-        submitError: undefined        
+      query: "",
+      number: 5,
+      cuisine: "",
+      diet: "",
+      intolerances: "",
+      submitError: undefined,
     };
   },
 
@@ -50,19 +50,26 @@ export default {
     async Search() {
       try {
         console.log(this.query);
+        console.log(this.number);
         const response = await this.axios.get(
           // "https://test-for-3-2.herokuapp.com/user/Register",
-          this.$root.store.server_domain + "/recipes/search",
-
-          {
-            query: this.query,
-            number: this.number,
-            cuisine: this.cuisine,
-            diet: this.diet,
-            intolerances: this.intolerances,
-          }
+          this.$root.store.server_domain + "/recipes/search?searchQuery=" + this.query + "&number=" + this.number + "&cuisine=" + this.cuisine + "&diet=" + this.diet + "&intolerances=" + this.intolerances        
         );
+
         console.log(response);
+
+        const recipes_id = response.data;
+        const recipes = [];
+
+        for (let i = 0; i < this.number; i++) {
+          console.log(recipes_id[i]);
+          recipes[i] = await this.axios.get(
+            // "https://test-for-3-2.herokuapp.com/user/Register",
+            this.$root.store.server_domain + "/recipes/:recipeId?id=" + recipes_id[i]
+          );
+        }
+
+        console.log(recipes);
       } catch (err) {
         console.log(err.response);
         this.submitError = err.response.data.message;
